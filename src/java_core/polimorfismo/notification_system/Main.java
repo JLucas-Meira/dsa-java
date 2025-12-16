b5b302f3df78e0bb2e3593d552199a335d275a6f
João Lucas Meira <joaolcs842@gmail.com>
2025-12-16 20:23:50 -0300

    fix: Corrige formatação de strings e adiciona log no loop principal.

--- src/java_core/polimorfismo/notification_system/Main.java
+++ src/java_core/polimorfismo/notification_system/Main.java
@@ -5,15 +5,18 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Notification> list = new ArrayList<>();
-        list.add(new Email("joao@gmail.com", "Olá, temos vagas abertas"));
-        list.add(new SMS("+55 81 99830-3306", "Você recebeu uma recarga"));
+        list.add(new Email("joao@gmail.com", "Welcome to JAVA!!!"));
+        list.add(new SMS("+55 81 99830-3306", "Your verify code is 734210"));
 
+        //Percorre a lista de notificações aplicando polimorfismo.
         for (Notification n : list) {
+            n.logRegister();
             n.send();
 
+            //Verifica o tipo específico de notificação (Recurso do Java 21)
             switch (n) {
-                case Email e -> System.out.println("LOG: Email has subject " + e.getDestination());
-                case SMS s -> System.out.println("LOG: SMS has subject " + s.getPhoneNumber());
+                case Email e -> System.out.println("LOG: Email has subject " + e.getSubject());
+                case SMS s -> System.out.println("LOG: SMS has subject " + s.getSubject());
                 default -> System.out.println("Unknown type");
             }
             System.out.println("-------------------------------------------------");
