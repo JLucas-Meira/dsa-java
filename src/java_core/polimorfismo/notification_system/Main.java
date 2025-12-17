ebaefd622f7a971696fa9978b69e61230d7febb0
João Lucas Meira <joaolcs842@gmail.com>
2025-12-16 22:16:42 -0300

    feat: Adiciona exemplos de Stream API e Lambda.

--- src/java_core/polimorfismo/notification_system/Main.java
+++ src/java_core/polimorfismo/notification_system/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         List<Notification> list = new ArrayList<>();
         list.add(new Email("joao@gmail.com", "Welcome to JAVA!!!"));
+        list.add(new Email("jose@hotmail.com", "You are learning Stream in Java."));
         list.add(new SMS("+55 81 99830-3306", "Your verify code is 734210"));
 
         //Percorre a lista de notificações aplicando polimorfismo.
-        for (Notification n : list) {
+         for (Notification n : list) {
             n.logRegister();
             n.send();
 
@@ -21,6 +22,23 @@ public class Main {
             }
             System.out.println("-------------------------------------------------");
         }
+
+        System.out.println("--- STREAM API ---");
+        System.out.println("Fltering only EMAILS for sending:");
+
+        list.stream()
+                .filter(n -> n instanceof Email) //Só passa se for Email
+                .forEach(n -> { //Ação para quem passou
+                    n.send();
+                    System.out.println("Processed by Stream");
+                });
+
+        //Conta quantos sms tem na lista.
+        long totalSMS = list.stream()
+                .filter(n -> n instanceof SMS)
+                .count();
+
+        System.out.println("\nTotal SMS in the list:" + totalSMS);
 
     }
 }
