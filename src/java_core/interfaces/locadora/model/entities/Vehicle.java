1a755f9764559148c1d8e8805d1b46d591f58aa3
João Lucas Meira <joaolcs842@gmail.com>
2026-02-09 20:49:16 -0300

    feat: implementa entidades de domínio básicas (Vehicle e CarRental)

---
+++ src/java_core/interfaces/locadora/model/entities/Vehicle.java
@@ -0,0 +1,21 @@
+package java_core.interfaces.locadora.model.entities;
+
+public class Vehicle {
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
+
