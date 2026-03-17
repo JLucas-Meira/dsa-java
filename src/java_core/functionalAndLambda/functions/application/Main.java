fc35c3b5874351cdb6e4369fe1fdd4db121617fb
João Lucas Meira <joaolcs842@gmail.com>
2026-03-17 11:07:24 -0300

    feat: Implementa function usando interface

---
+++ src/java_core/functionalAndLambda/functions/application/Main.java
@@ -0,0 +1,26 @@
+package java_core.functionalAndLambda.functions.application;
+
+import java_core.functionalAndLambda.functions.entities.Product;
+import java_core.functionalAndLambda.functions.util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1299.00));
+        list.add(new Product("HD Case", 85.00));
+        list.add(new Product("Mouse", 59.90));
+
+        //Uso do function com interface
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+
+    }
+}
---
+++ src/java_core/functionalAndLambda/functions/util/UpperCaseName.java
@@ -0,0 +1,13 @@
+package java_core.functionalAndLambda.functions.util;
+
+import java_core.functionalAndLambda.functions.entities.Product;
+
+import java.util.function.Function;
+
+public class UpperCaseName implements Function<Product, String> {
+
+    @Override
+    public String apply(Product product) {
+        return product.getName().toUpperCase();
+    }
+}
